085cb282f4b330d000e0d694c7da13bfc45a242f
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-16 22:17:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,6 +8,6 @@ def directors_totals(nds)
         director = nds[directors_index][:name]
     movie_index = 0
       result[director] = 0 
-        while movie_index < nds[directors_index][:movies].length do
+        while movie_index < nds[directors_index][:movies].length do 
   nil
 end
